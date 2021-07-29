6c0669be33891bbef27fdb1f616492f078ba0054
Matheus Muniz <matheusmuniz100@hotmail.com>
2021-07-29 12:57:06 -0300

    para fixar 01

---
+++ Fundamentos/Bloco-04_Introducao-a-JavaScript-e-Logica-Programacao/Dia-01_JavaScript_Primeiros-Passos/para-fixar/para-fixar-01.js
@@ -0,0 +1,15 @@
+const myName = "Matheus";
+
+const birthCity = "Juiz de Fora";
+
+let birthYear = "2001";
+
+birthYear = 2030;
+
+birthCity = "Belo Horizonte";
+
+console.log(myName);
+
+console.log(birthCity);
+
+console.log(birthYear);
