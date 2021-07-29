c584779ca9c8d5f8b3174bfe9250b4c017e4ee1d
Matheus Muniz <matheusmuniz100@hotmail.com>
2021-07-29 13:21:34 -0300

    adicionar para-fixar-02

---
+++ Fundamentos/Bloco-04_Introducao-a-JavaScript-e-Logica-Programacao/Dia-01_JavaScript_Primeiros-Passos/para-fixar/para-fixar-02.js
@@ -0,0 +1,23 @@
+// Parte Inicial:
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientId);
+
+// console.log(typeof patientEmail);
+
+// console.log(typeof patientInfo);
+
+let base = 5;
+let altura = 8;
+
+let area = base * altura;
+console.log(area);
+
+let perimetro = (base*2) + (altura*2);
+console.log(perimetro);
