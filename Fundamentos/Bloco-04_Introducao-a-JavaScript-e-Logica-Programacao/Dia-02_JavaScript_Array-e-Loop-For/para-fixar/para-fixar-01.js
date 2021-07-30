45f295161e8c605982f1b1d3e3df8d795b0d4200
Matheus Muniz <matheusmuniz100@hotmail.com>
2021-07-30 12:52:22 -0300

    para-fixar-01

---
+++ Fundamentos/Bloco-04_Introducao-a-JavaScript-e-Logica-Programacao/Dia-02_JavaScript_Array-e-Loop-For/para-fixar/para-fixar-01.js
@@ -0,0 +1,22 @@
+ //EXERCICIO 01:
+ let menu = ["Home", "Serviços", "Portfólio", "Links"];
+ let menuServices = menu[1];
+
+ console.log(menuServices);
+
+
+
+
+ //EXERCICIO 02:
+ let menu = ["Home", "Serviços", "Portfólio", "Links"];
+ let indexPortfolio = menu.indexOf("Portfólio");
+
+ console.log(indexPortfolio);
+
+
+
+ //EXERCICIO 03:
+ let menu = ["Home", "Serviços", "Portfólio", "Links"];
+ menu.push("Contato");
+
+ console.log(menu);
