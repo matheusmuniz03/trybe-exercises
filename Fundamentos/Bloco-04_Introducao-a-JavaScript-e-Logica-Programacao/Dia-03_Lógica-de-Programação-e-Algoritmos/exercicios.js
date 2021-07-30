817c5ca1f6b20dc4211f48f1e748b434b0adbdca
Matheus Muniz <matheusmuniz100@hotmail.com>
2021-07-30 18:20:41 -0300

    exercicios/4.3

---
+++ Fundamentos/Bloco-04_Introducao-a-JavaScript-e-Logica-Programacao/Dia-03_Lógica-de-Programação-e-Algoritmos/exercicios.js
@@ -0,0 +1,48 @@
+//EXERCICIO 01:
+// let valor = 5;
+// let linha = "";
+
+// for (let indexAux = 0; indexAux < valor; indexAux += 1)
+// {
+//     linha = linha + "*";
+// }
+// for (let index = 0; index < valor; index += 1)
+// {
+//     console.log(linha);
+// }
+
+
+
+//EXERCICIO 02:
+// let valor = 5;
+// let impressão = "*";
+
+// for(let index = 0; index < valor; index += 1){
+//     console.log(impressão);
+//     impressão = impressão + "*";
+// }
+
+
+
+//EXERCICIO 03:
+// let valor = 5;
+// let impressão = "";
+// let contador = valor;
+
+// for (let index = 0; index < valor; index += 1){
+//     for (let indexAux = 0; indexAux <= valor; indexAux += 1){
+//         if (indexAux < contador){
+//             impressão = impressão + " ";
+//         }
+//         else{
+//             impressão = impressão + "*";
+//         }
+//     }
+//     console.log(impressão);
+//     impressão = "";
+//     contador -= 1;
+// }
+
+
+
+//EXERCICIO 04:
