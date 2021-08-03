548e2ff51cfbdcce6bcc768620f60d354d0528fe
Matheus Muniz <matheusmuniz100@hotmail.com>
2021-08-03 10:50:33 -0300

    exercicios 4.3

--- Fundamentos/Bloco-04_Introducao-a-JavaScript-e-Logica-Programacao/Dia-03_Lógica-de-Programação-e-Algoritmos/exercicios.js
+++ Fundamentos/Bloco-04_Introducao-a-JavaScript-e-Logica-Programacao/Dia-03_Lógica-de-Programação-e-Algoritmos/exercicios.js
@@ -1,48 +1,72 @@
 //EXERCICIO 01:
-// let valor = 5;
-// let linha = "";
+let valor = 5;
+let linha = "";
 
-// for (let indexAux = 0; indexAux < valor; indexAux += 1)
-// {
-//     linha = linha + "*";
-// }
-// for (let index = 0; index < valor; index += 1)
-// {
-//     console.log(linha);
-// }
+for (let indexAux = 0; indexAux < valor; indexAux += 1)
+{
+    linha = linha + "*";
+}
+for (let index = 0; index < valor; index += 1)
+{
+    console.log(linha);
+}
 
 
 
 //EXERCICIO 02:
-// let valor = 5;
-// let impressão = "*";
+let valor = 5;
+let impressão = "*";
 
-// for(let index = 0; index < valor; index += 1){
-//     console.log(impressão);
-//     impressão = impressão + "*";
-// }
+for(let index = 0; index < valor; index += 1){
+    console.log(impressão);
+    impressão = impressão + "*";
+}
 
 
 
 //EXERCICIO 03:
-// let valor = 5;
-// let impressão = "";
-// let contador = valor;
+let valor = 5;
+let impressão = "";
+let contador = valor;
 
-// for (let index = 0; index < valor; index += 1){
-//     for (let indexAux = 0; indexAux <= valor; indexAux += 1){
-//         if (indexAux < contador){
-//             impressão = impressão + " ";
-//         }
-//         else{
-//             impressão = impressão + "*";
-//         }
-//     }
-//     console.log(impressão);
-//     impressão = "";
-//     contador -= 1;
-// }
+for (let index = 0; index < valor; index += 1){
+    for (let indexAux = 0; indexAux <= valor; indexAux += 1){
+        if (indexAux < contador){
+            impressão = impressão + " ";
+        }
+        else{
+            impressão = impressão + "*";
+        }
+    }
+    console.log(impressão);
+    impressão = "";
+    contador -= 1;
+}
 
 
 
 //EXERCICIO 04:
+let valor = 11;
+let impressão = '';
+let meioTriangulo =  (valor + 1) / 2;
+let ladoEsquerdo = meioTriangulo;
+let ladoDireito = meioTriangulo;
+
+for (let index = 0; index <= meioTriangulo; index += 1)
+{
+    for (let indexAux = 0; indexAux <= valor; indexAux += 1)
+    {
+        if (indexAux > ladoEsquerdo && indexAux < ladoDireito)
+        {
+            impressão = impressão + "*";
+        }
+        else
+        {
+            impressão = impressão + " ";
+        }
+    }
+    console.log(impressão);
+    impressão = "";
+    ladoDireito += 1;
+    ladoEsquerdo -= 1;
+}
