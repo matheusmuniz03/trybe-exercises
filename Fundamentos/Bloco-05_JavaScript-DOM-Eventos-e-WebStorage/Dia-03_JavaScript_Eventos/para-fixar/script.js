ac363785014a9d2a385e087babd82b79840c3ec0
Matheus Muniz <matheusmuniz100@hotmail.com>
2021-08-18 14:29:11 -0300

    Bloco 05 - Dia 03

---
+++ Fundamentos/Bloco-05_JavaScript-DOM-Eventos-e-WebStorage/Dia-03_JavaScript_Eventos/para-fixar/script.js
@@ -0,0 +1,54 @@
+let firstLi = document.getElementById('first-li');
+let secondLi = document.getElementById('second-li');
+let thirdLi = document.getElementById('third-li');
+let input = document.getElementById('input');
+let myWebpage = document.getElementById('my-spotrybefy');
+
+/*
+1. Copie esse arquivo e edite apenas ele;
+
+2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
+2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+
+
+3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+com a classe 'tech';
+
+4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+redirecione para alguma página;
+4.1. Que tal redirecionar para seu portifólio?
+
+5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+a cor do mesmo;
+
+Segue abaixo um exemplo do uso de event.target:
+*/
+
+function resetText(event) {
+  // O Event é passado como um parâmetro para a função.
+  event.target.innerText = 'Opção reiniciada';
+  // O event possui várias propriedades, porém a mais usada é o event.target,
+  // que retorna o objeto que disparou o evento.
+}
+
+firstLi.addEventListener('dblclick', resetText);
+
+// Não precisa passar o parâmetro dentro da callback resetText. O próprio
+// navegador fará esse trabalho por você, não é legal? Desse jeito, o
+// event.target na nossa função retornará o objeto 'firstLi'.
+
+//Exercicio 02:
+
+
+
+//Exercicio 03:
+function handleChangeTech(event) {
+    const techElement = document.querySelector('.tech');
+    techElement.classList.remove('tech');
+    event.target.classList.add('tech');
+    input.value = '';
+  }
+  
+  firstLi.addEventListener('click', handleChangeTech);
+  secondLi.addEventListener('click', handleChangeTech);
+  thirdLi.addEventListener('click', handleChangeTech);
